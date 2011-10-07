6011fbe9a105c6bbf2c585424691b03ca877d561
DooMLoRD <metoo.mememe@gmail.com>
2011-10-08 02:13:48 +0530

    first commit

---
+++ main_kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Sat Oct 8 01:28:59 IST 2011"
+#define LINUX_COMPILE_TIME "01:28:59"
+#define LINUX_COMPILE_BY "doomlord"
+#define LINUX_COMPILE_HOST "doombox"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
